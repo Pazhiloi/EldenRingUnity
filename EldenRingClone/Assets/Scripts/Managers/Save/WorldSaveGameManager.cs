6fca964108901a297afebc009a43e06bb05d2afe
Miroslav Miroslavov <martinsen06@gmail.com>
2025-05-14 10:27:52 +0300

    1 Netcode For Gameobjects

---
+++ EldenRingClone/Assets/Scripts/Managers/Save/WorldSaveGameManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MR
+{
+  public class WorldSaveGameManager : MonoBehaviour
+  {
+    public static WorldSaveGameManager instance;
+    [SerializeField] private int worldSceneIndex = 1;
+    private void Awake()
+    {
+      InitSingleton();
+    }
+
+    private void Start()
+    {
+      DontDestroyOnLoad(gameObject);
+    }
+
+
+public IEnumerator LoadNewGame(){
+  AsyncOperation loadOperation = SceneManager.LoadSceneAsync(worldSceneIndex);
+  yield return null;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+    private void InitSingleton()
+    {
+      if (instance == null)
+      {
+        instance = this;
+      }
+      else
+      {
+        Destroy(gameObject);
+      }
+    }
+  }
+}
---
+++ EldenRingClone/Assets/Scripts/Managers/UI/PlayerUIManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+namespace MR
+{
+    public class PlayerUIManager : MonoBehaviour
+    {
+    public static PlayerUIManager instance;
+    [Header("NETWORK JOIN")]
+    [SerializeField] bool startGameAsClient;
+
+
+    private void Awake() {
+      InitSingleton();
+    }
+
+    private void Start() {
+      DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+      if (startGameAsClient)
+      {
+        startGameAsClient = false;
+        // WE MUST FIRST SHUT DOWN, BECAUSE WE HAVE STARTED AS A HOST DURING THE TITLE SCREEN
+        NetworkManager.Singleton.Shutdown();
+        // WE THEN RESTART, AS A CLIENT
+        NetworkManager.Singleton.StartClient();
+      }
+    }
+
+
+
+
+
+
+
+
+
+    private void InitSingleton()
+    {
+      if (instance == null)
+      {
+        instance = this;
+      }
+      else
+      {
+        Destroy(gameObject);
+      }
+    }
+  }
+}
